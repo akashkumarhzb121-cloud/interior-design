514b7f2ab77c6e12b3095f2a207553cbc077e072
Akashkumarr <akashkumarhzb121@gmail.com>
2026-06-07 19:50:14 +0530

    Simplify error handling in axios response interceptor by removing redundant token check for 401 status

--- src/api/axios.js
+++ src/api/axios.js
@@ -32,11 +32,8 @@ api.interceptors.response.use(
   (response) => response,
   (error) => {
     if (error.response?.status === 401) {
-      const token = localStorage.getItem('admin_token')
-      if (token) {
-        localStorage.removeItem('admin_token')
-        window.location.href = '/admin/login'
-      }
+      localStorage.removeItem('admin_token')
+      window.location.href = '/admin/login'
     }
     return Promise.reject(error)
   }
